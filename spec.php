f9d6720bed64b5212a3976bfed9fe218c082ac69
TonyHuang <hzx0109@gmail.com>
2024-11-18 14:37:10 -0800

    update spec page's style

--- spec.php
+++ spec.php
@@ -8,6 +8,10 @@
                 margin: 10px 0 !important;
                 /* max-width: 250px; */
             }
+
+            p {
+                margin-bottom: 4px !important;
+            }
         </style>
     </head>
     <body>
@@ -28,8 +32,8 @@
                             <div class="spec-wrap">
                                 <h3>Display</h3>
                                 <hr>
-                                <p>Size: 5.45 inch TFT LCD</p>
-                                <p>Resolution: 480x960FW+IPS</p>
+                                <p><strong>Size: </strong>5.45 inch TFT LCD</p>
+                                <p><strong>Resolution: </strong>480x960FW+IPS</p>
                             </div>
                         </div>
                         <div class="col-6 col-md-4">
@@ -44,41 +48,41 @@
                             <div class="spec-wrap">
                                 <h3>Camera</h3>
                                 <hr>
-                                <p>Front Camera: 2MP</p>
-                                <p>Rear Camera: 5MP with LED</p>
+                                <p><strong>Front Camera: </strong>2MP</p>
+                                <p><strong>Rear Camera: </strong>5MP with LED</p>
                             </div>
                         </div>
                         <div class="col-6 col-md-4">
                             <div class="spec-wrap">
                                 <h3>Memory & Storage</h3>
                                 <hr>
-                                <p>RAM: 3 GB</p>
-                                <p>ROM: 32 GB</p>
+                                <p><strong>RAM: </strong>3 GB</p>
+                                <p><strong>ROM: </strong>32 GB</p>
                             </div>
                         </div>
                         <div class="col-6 col-md-4">
                             <div class="spec-wrap">
                                 <h3>Networks</h3>
                                 <hr>
-                                <p>2G: B2/B3/B5/B8</p>
-                                <p>3G: B1/B2/B4/B5/B8</p>
-                                <p style="overflow-wrap: break-word;">4G: B1/<wbr>B2/<wbr>B3/<wbr>B4/<wbr>B5/<wbr>B7/<wbr>B8/<wbr>B12/<wbr>B17/<wbr>B20/<wbr>B25/<wbr>B26/<wbr>B34/<wbr>B38/<wbr>B39/<wbr>B40/<wbr>B41/<wbr>B66/<wbr>B71</p>
+                                <p><strong>2G: </strong>B2/B3/B5/B8</p>
+                                <p><strong>3G: </strong>B1/B2/B4/B5/B8</p>
+                                <p style="overflow-wrap: break-word;"><strong>4G: </strong>B1/<wbr>B2/<wbr>B3/<wbr>B4/<wbr>B5/<wbr>B7/<wbr>B8/<wbr>B12/<wbr>B17/<wbr>B20/<wbr>B25/<wbr>B26/<wbr>B34/<wbr>B38/<wbr>B39/<wbr>B40/<wbr>B41/<wbr>B66/<wbr>B71</p>
                             </div>
                         </div>
                         <div class="col-6 col-md-4">
                             <div class="spec-wrap">
                                 <h3>Connectivity</h3>
                                 <hr>
-                                <p>WiFi: 5 802.11a/b/g/n/ac</p>
-                                <p>Bluetooth: 5.2</p>
-                                <p>Location: GPS/BDS/Glonass/Galileo</p>
+                                <p><strong>Wi-Fi: </strong>5 802.11a/b/g/n/ac</p>
+                                <p><strong>Bluetooth: </strong>5.2</p>
+                                <p><strong>Location: </strong>GPS/BDS/Glonass/Galileo</p>
                             </div>
                         </div>
                         <div class="col-6 col-md-4">
                             <div class="spec-wrap">
                                 <h3>Battery & Charging</h3>
                                 <hr>
-                                <p>Battery: 2500mAh</p>
+                                <p><strong>Battery: </strong>2500mAh</p>
                                 <p>5V/1A charging</p>
                                 <p>USB Type-C</p>
                             </div>
@@ -94,8 +98,8 @@
                             <div class="spec-wrap">
                                 <h3>Others</h3>
                                 <hr>
-                                <p>3.5mm</p>
-                                <p>Rear Camera: 5MP with LED</p>
+                                <p>3.5mm Headphone Jack</p>
+                                <p>Detachable Battery</p>
                             </div>
                         </div>
                     </div>
