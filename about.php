8722d120ac4731d96329b21af4155d5ad3dad77f
TonyHuang <hzx0109@gmail.com>
2024-12-11 11:54:27 -0800

    update about page

--- about.php
+++ about.php
@@ -3,34 +3,39 @@
     <head>
         <?php include './inc/HTMLhead.php'; ?>
     </head>
-    <body>
+    <body style="overflow-x: hidden;">
         <?php include './inc/header.php'; ?>
 
-        <div class="banner-container" id="overview">
-            <img src="https://dummyimage.com/3000x2000" alt="banner" class="img-fluid banner">
-            <div class="container phone-banner-text d-flex align-items-center" style="height: 400px;">
-                <div>
+        <div class="about-banner-container">
+            <div class="container">
                     <p>About Us</p>
                     <div class="heading">Technology for All, Connect Communities</div>
-                </div>
             </div>
         </div>
 
         <div class="container section-padding">
-            <h2 class="text-center bottom-padding">INOVA mobile phone headquartered in British Columbia, Canada. Our Mission is to create mobile devices with great design, high quality at affordable prices.</h2>
-        <!-- </div>
+            <div class="about-title text-center bottom-padding">INOVA was born from the idea that technology should be accessible and bring people closer. Our journey is guided by a commitment to affordability and community support, as we strive to create products that empower our users and foster connections between users and their communities.</div>
 
-        <div class="container section-padding"> -->
-            <div class="position-relative">
-                <img src="https://dummyimage.com/3000x1000" alt="in the box" class="img-fluid" style="border-radius: 20px;">
+            <div class="about-banner2-container">
                 <div class="row">
-                    <div class="position-absolute top-50 start-0 translate-middle-y">
                         <div class="col-12 col-md-6" style="padding-left: 50px;">
-                            <h2>We are committed to maximize value. That includes powerful technology, durability, and support at a great price.</h2>
+                            <div class="about-title">We are committed to maximize value. That includes powerful technology, durability, and support at a great price.</div>
                         </div>
-                    </div>
                 </div>
             </div>
+
+            <div class="row gx-5" style="margin-top: 100px;">
+                <div class="col-md-6">
+                    <div class="about-title">OUR MISSION</div>
+                    <p>Our mission is to deliver affordable, high-quality tech solutions that empower communities to come together through our innovative products.</p>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="about-title">OUR VISION</div>
+                    <p>Our vision is to create a world where innovative, accessible technology enables everyone to seamlessly stay connected no matter where they are.</p>
+                </div>
+            </div>
+
         </div>
         
         <?php include './inc/footer.php'; ?>
